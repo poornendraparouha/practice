1bdbbbb4121967716865838748c01aabff9b8c98
poornendra parouha <poornendraparouha@gmail.com>
2025-01-10 23:01:09 +0530

    added middlewares

---
+++ express/express-server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+// create a server
+const server = express();
+// define the port
+const port = 3000;
+// define the route(handle the default request)
+server.get('/', (req, res, next) => {
+    console.log('first middleware!');
+    next();
+});
+
+server.get('/', (req, res) => {
+    res.send('Welcome to express server!');
+});
+
+// start the server
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
